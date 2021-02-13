1cb83e72082fb7811161fab7aad6650dd86c8413
Rafael Gil <rafael.gil@sapo.pt>
2021-02-13 12:30:42 +0000

    Replaced redux-devtools components with redux-devtools-extension to enable the redux browser extensions

--- src/main/webapp/app/config/store.ts
+++ src/main/webapp/app/config/store.ts
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunkMiddleware from 'redux-thunk';
 import reducer, { IRootState } from 'app/shared/reducers';
-import DevTools from './devtools';
 import errorMiddleware from './error-middleware';
 import notificationMiddleware from './notification-middleware';
 import loggerMiddleware from './logger-middleware';
 import { loadingBarMiddleware } from 'react-redux-loading-bar';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const defaultMiddlewares = [
   thunkMiddleware,
@@ -16,9 +16,10 @@ const defaultMiddlewares = [
   loadingBarMiddleware(),
   loggerMiddleware,
 ];
+
 const composedMiddlewares = middlewares =>
   process.env.NODE_ENV === 'development'
-    ? compose(applyMiddleware(...defaultMiddlewares, ...middlewares), DevTools.instrument())
+    ? composeWithDevTools(applyMiddleware(...defaultMiddlewares, ...middlewares)) // , DevTools.instrument())
     : compose(applyMiddleware(...defaultMiddlewares, ...middlewares));
 
 const initialize = (initialState?: IRootState, middlewares = []) => createStore(reducer, initialState, composedMiddlewares(middlewares));
